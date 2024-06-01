42379998abbc8cc73b746132dee2456f3d32ceca
hosogaya <hosogaya.hirotaka@gmail.com>
2024-06-01 17:34:44 +0900

    add gitignore

--- build/sdf_generator/CMakeFiles/sdf_generator_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sdf_generator_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sdf_generator_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
